8ade69ebf02d7b42070fcec620369fb584b8ae20
SkylarGoodwin <JamesSkylarGoodwin@gmail.com>
2024-04-03 13:23:41 -0500

    initial commit

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the number of wheels for your vehicle");
+            int wheelCount;
+            var winput = int.TryParse(Console.ReadLine(), out wheelCount);
+
+            var vehicle = VehicleFactory.GetVehicle(wheelCount);
+            vehicle.Drive();
         }
     }
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    internal static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int wheelCount)
+        {
+            if(wheelCount == 2)
+            {
+                return new Motorcycle();
+            }
+            else { return new Car(); }
+        }
+    }
+}
